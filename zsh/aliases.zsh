32931296143ebcc528bed56eca1adcefa616a86c
anymoto <ana@castromartinez.com>
2014-03-12 19:11:23 -0600

    Initial commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,28 @@
+# -----------------------------------------------------------------------
+# Ruby stuff
+# -----------------------------------------------------------------------
+alias rake='noglob rake' # necessary to make rake work inside of zsh
+alias be='bundle exec'
+alias bu='bundle update'
+
+# -----------------------------------------------------------------------
+# Directory movement
+# -----------------------------------------------------------------------
+alias ..='cd ..'
+
+# directory shortcuts
+alias projects='~/Projects'
+
+# -----------------------------------------------------------------------
+# Git
+# -----------------------------------------------------------------------
+alias ga='git add'
+alias gp='git push'
+alias gpl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)
+<%an>%Creset' --abbrev-commit"
+alias gst='git status'
+alias gd='git diff'
+alias go='git checkout'
+alias gbr='git branch'
+alias gpoh='git push origin head'
+alias glog='git log --oneline --decorate --graph --all'
